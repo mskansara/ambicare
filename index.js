bad443cfca9e37fa8df92c8aa47dad339fd23ece
Manthan Kansara <manthankansara7@gmail.com>
2019-03-06 13:51:29 +0530

    Install express, mysql and nodemon, set up index.js file

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}`));
